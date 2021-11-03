2f2894d58704021aa9cf9b8d0b54461bfb5eb265
TadjhBrooks <tadjh.brooks@gmail.com>
2021-11-02 18:42:25 -0700

    Add time

--- commands/cl_time.lua
+++ commands/cl_time.lua
@@ -1,11 +1,16 @@
+-- Change time of day to Daytime or input an specific hour of day
 function ChangeTime(source, args)
-    local time = args[1]
-    print(args)
+    local time = 12
+
+    if not args[1] == nil then
+        time = tonumber(args[1])
+    end
+
     if time and (time < 0 or time > 23) then
         print('Invalid hour of day')
         return
     end
-    local hour = time or 0
-    NetworkOverrideClockTime(hour, 0, 0);
+
+    NetworkOverrideClockTime(time, 0, 0);
 end
 RegisterCommand('time', ChangeTime, false)
