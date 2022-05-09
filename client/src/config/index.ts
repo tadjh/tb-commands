3db98df6bc9b247b4ee1b45a308174d2ec37f348
Tadjh <tadjh.brooks@gmail.com>
2022-05-08 17:47:14 -0700

    fix: broken import

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,6 @@
 export const CURRENT_RESOURCE_NAME = GetCurrentResourceName();
 
+export const COMMAND_CAR = "car";
 export const COMMAND_COORDS = "coords";
 export const COMMAND_PED = "ped";
 export const COMMAND_RESPAWN = "respawn";
--- client/src/features/car/config/index.ts
+++ client/src/features/car/config/index.ts
@@ -1,3 +1,2 @@
-export const COMMAND_CAR = "car";
 export const DEFAULT_SEAT = -1;
 export const DEFAULT_VEHICLE = "turismo2";
