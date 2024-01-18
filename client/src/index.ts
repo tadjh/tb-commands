4c4d189ad40d8616d4459c00977931b2384342f3
tadjh <tadjh.brooks@gmail.com>
2024-01-18 14:24:16 -0800

    Remove time

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,14 +3,14 @@ import {
   COMMAND_COORDS,
   COMMAND_PED,
   COMMAND_RESPAWN,
-  COMMAND_TIME,
+  // COMMAND_TIME,
   COMMAND_TP,
 } from "./config";
 import { vehicle } from "./features/vehicle";
 import { coords } from "./features/coords";
 import { ped } from "./features/ped";
 import { respawn } from "./features/respawn";
-import { time } from "./features/time";
+// import { time } from "./features/time";
 import { tp } from "./features/tp";
 import { SpawnVehicle } from "./features/vehicle/utils/natives";
 import { SetPedModel } from "./features/ped/utils/natives";
@@ -19,7 +19,7 @@ RegisterCommand(COMMAND_COORDS, coords, false);
 RegisterCommand(COMMAND_VEHICLE, vehicle, false);
 RegisterCommand(COMMAND_PED, ped, false);
 RegisterCommand(COMMAND_RESPAWN, respawn, false);
-RegisterCommand(COMMAND_TIME, time, false);
+// RegisterCommand(COMMAND_TIME, time, false);
 RegisterCommand(COMMAND_TP, tp, false);
 
 globalThis.exports("SpawnVehicle", SpawnVehicle);
