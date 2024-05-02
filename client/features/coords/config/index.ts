4962d4d410f0c75e93c319fea3889310458495ba
tadjh <tadjh.brooks@gmail.com>
2024-05-01 17:41:00 -0700

    Simplify coords

--- client/features/coords/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_COORDS_LABEL = "coords";
--- client/features/coords/index.ts
+++ client/features/coords/index.ts
@@ -1,11 +1,8 @@
-import { DEFAULT_COORDS_LABEL } from "./config";
-
 /**
  * Gets the ped's current coordinates
  * @param _source The source (unused)
  * @returns void
  */
 export function coords(_source: number) {
-  const pedCoords = GetEntityCoords(PlayerPedId(), true);
-  console.log(DEFAULT_COORDS_LABEL, ...pedCoords);
+  console.log("coords", ...GetEntityCoords(PlayerPedId(), true));
 }
